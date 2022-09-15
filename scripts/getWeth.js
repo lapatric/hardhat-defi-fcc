674d32bd3a3f7ab540ffbcd18d014cd2051402cb
lapatric <42653152+lapatric@users.noreply.github.com>
2022-09-15 10:05:37 +0200

    add small comment

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -6,6 +6,7 @@ async function getWeth() {
 
     const { deployer } = await getNamedAccounts();
     // Mainnet WETH address: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 
+    // WETH is an ERC20 token with the additional functions of deposit and withdraw
     const iWeth = await ethers.getContractAt("IWeth", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", deployer);
     const tx = await iWeth. deposit({ value: AMOUNT });
     await tx.wait(1);
